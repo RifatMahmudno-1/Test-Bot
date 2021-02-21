6ead9cf2a22eb4561d1ce23b4a926360f60b1eab
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-02-21 19:29:10 +0600

    me

--- bot.js
+++ bot.js
@@ -110,8 +110,8 @@ async function gotMessage(msg) {
         } else if (tokens[0] === 'help') {
             msg.channel.send(
                 `**Type**
-            **(1)** '${prefix}hi' or '${prefix}hello' for greetings accoeding to GMT+6
-            **(2)** '${prefix}hi <GMT>' or '${prefix}hello <GMT>' for greetings accoeding to provided GMT such as '${prefix}hi +3' '${prefix}hello -5'
+            **(1)** '${prefix}hi' or '${prefix}hello' for greetings according to GMT+6
+            **(2)** '${prefix}hi <GMT>' or '${prefix}hello <GMT>' for greetings according to provided GMT such as '${prefix}hi +3' '${prefix}hello -5'
             **(3)** '${prefix}gif <search>' for gif such as '${prefix}gif dog' '${prefix}gif cute kitten'
             **(4)** '${prefix}time' to get time of GMT+6
             **(5)** '${prefix}time <GMT>' such as '${prefix}time +6' or '${prefix}time +8' etc to get your timezone's time.
@@ -119,7 +119,12 @@ async function gotMessage(msg) {
             **(7)** if you type '${prefix}bhf <@mension> <Type your text>' then the bot will say on your behalf. Such as- '${prefix}bhf  @Rifat is admin' etc.
             **(8)** if you type '${prefix}avt' then the bot will reply your avatar.
             **(9)** if you type '${prefix}avt @mention @mention ...' then the bot will reply their avater.
-            **(10)** if you type '${prefix}cha <channel name> <Your message>' then the bot will send that message to that specific channel. Such as- '${prefix}cha general How are you??' etc..`
+            **(10)** if you type '${prefix}cha <channel name> <Your message>' then the bot will send that message to that specific channel. Such as- '${prefix}cha general How are you??' etc..
+            **(11)** type '${prefix}kick <reason in one word. For more words join them by hyfen or underscore> <Mention them whom you want to kick>'. Such as-'${prefix}kick for_testing_purpose @Rifat @Anilist Bot @My Test'
+            ** (12) ** type '${prefix}ban <reason in one word. For more words join them by hyfen or underscore> <Mention them whom you want to ban>'.Such as - '${prefix}ban for_testing_purpose @Rifat @Anilist Bot @My Test'
+            **(13)** type '${prefix}snick <mention user whose nickname you want to change> <nickname>'. Such as- '${prefix}snick @Rifat admin' or, '${prefix}snick @Rifat admin @My test BOT' etc.. 
+            **(14)**type '${prefix}rnick <mention user which nickname you want to remove>'. Such as- '${prefix}rnick @admin @BOT' etc..
+            `
             )
         } else if (tokens[0] === 'time') {
             if (tokens.length == 1) {
@@ -184,6 +189,56 @@ async function gotMessage(msg) {
                 chann.get(idd).send(`${keywords}`);
             } else {
                 msg.reply(`Type Correctly. For help type '${prefix}help'`)
+            }
+        } else if (tokens[0] === 'kick') {
+            if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('KICK_MEMBERS')) {
+                if (tokens.length > 1) {
+                    let userArray = msg.mentions.users.array();
+                    userArray.forEach(function (el) {
+                        msg.guild.members.cache.get(el.id).kick()
+                        msg.channel.send(`${el.username} was kicked from this server successfully ${tokens[1]}.`)
+                    })
+                } else {
+                    msg.reply(`Type correctly. For help type '${prefix}help'`)
+                }
+            } else {
+                msg.reply(`You don't have permission to kick any user.`)
+            }
+        } else if (tokens[0] === 'ban') {
+            if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('KICK_MEMBERS')) {
+                if (tokens.length > 1) {
+                    let userArray = msg.mentions.users.array();
+                    userArray.forEach(function (el) {
+                        msg.channel.send(`${el.username} was banned from this server successfully ${tokens[1]}.`)
+                        msg.guild.members.cache.get(el.id).ban()
+                    })
+                } else {
+                    msg.reply(`Type correctly. For help type '${prefix}help'`)
+                }
+            } else {
+                msg.reply(`You don't have permission to ban any user.`)
+            }
+        } else if (tokens[0] === 'snick') {
+            if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('CHANGE_NICKNAME')) {
+                let userArray = msg.mentions.users.array();
+                let count = 0;
+                for (var i = 0; i < userArray.length; i++) {
+                    count += 2
+                    msg.guild.members.cache.get(userArray[i].id).setNickname(tokens[count])
+                    msg.channel.send(`I've set the nickname of ${userArray[i].username} to ${tokens[count]}`)
+                }
+            } else {
+                msg.reply(`You don't have permission to change nicknames.`)
+            }
+        } else if (tokens[0] === 'rnick') {
+            if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('CHANGE_NICKNAME')) {
+                let userArray = msg.mentions.users.array();
+                for (var i = 0; i < userArray.length; i++) {
+                    msg.guild.members.cache.get(userArray[i].id).setNickname(' ')
+                    msg.channel.send(`I've removed the nickname of ${userArray[i].username}`)
+                }
+            } else {
+                msg.reply(`You don't have permission to reset nicknames.`)
             }
         }
     }
