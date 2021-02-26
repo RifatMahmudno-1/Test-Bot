2d4bf188c02b42fa1910a0838153e6f3bd64e33d
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-02-26 11:39:03 +0000

    me

--- main/messageCommands/count.js
+++ main/messageCommands/count.js
@@ -6,7 +6,6 @@ module.exports = function (msg, counter, fs) {
         fs.writeFile('./main/messageCommands/counter.json', JSON.stringify(counter, null, 4), err => {
             if (err) throw err;
         })
-        msg.channel.send(`<@${msg.author.id}>, You have reached level 1.`)
     } else if (counter[msg.author.id] !== undefined) {
         counter[msg.author.id] = {
             count: counter[msg.author.id].count + 1
@@ -15,7 +14,7 @@ module.exports = function (msg, counter, fs) {
             if (err) throw err;
         })
         if (Number.isInteger((counter[msg.author.id].count) / 30) == true) {
-            msg.channel.send(`<@${msg.author.id}>, You have reached level ${((counter[msg.author.id].count) / 30) + 1}.`)
+            msg.channel.send(`<@${msg.author.id}>, You have reached level ${(counter[msg.author.id].count) / 30}.`)
         }
     }
 }
