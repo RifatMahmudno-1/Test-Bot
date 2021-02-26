cb2b64c909ec893d84f2999b06e7fde16b5517b8
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-02-26 10:54:12 +0000

    me

--- bot.js
+++ bot.js
@@ -4,11 +4,11 @@ const client = new Discord.Client();
 client.login(process.env.TOKEN)
 client.on('ready', readyDiscord)
 
-//const keepalive = require('./main/keepalive')
+const keepalive = require('./main/keepalive')
 
 function readyDiscord() {
     console.log('Application has started');
-    //keepalive.alive();
+    keepalive.alive();
 }
 const messageCommands = require('./main/messageCommands');
 client.on('message', messageCommands)
