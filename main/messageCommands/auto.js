7fdb9245546c935bc901b544ea2726f39aa29acf
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-03-06 00:19:36 +0600

    me

--- main/messageCommands/auto.js
+++ main/messageCommands/auto.js
@@ -1,11 +1,14 @@
 const greeting = require('./greeting.js')
-const badwords=require('./badwords.js')
+const badwords = require('./badwords.js')
 let emtypes = ['rich', 'image', 'video', 'gifv', 'article', 'link']
 module.exports = function (msg, tokens, prefix, otfn) {
+    var keywords = tokens.join(' ').toLowerCase()
+    keywords = keywords.split(' ');
+
     if (otfn.greew().includes(tokens[0])) {
         greeting(msg, tokens, prefix, otfn)
         msg.react(otfn.reac())
-    } else if (badwords.some(ai => tokens.includes(ai))) {
+    } else if (badwords.some(ai => keywords.includes(ai))) {
         msg.delete();
         msg.reply(`Please, don't use bad words.`)
     } else if (tokens.includes('like') || tokens.includes('love') || tokens.includes('adore') || tokens.includes('cute')) {
