329ed835897361642aa770a474be6d8e521be3c4
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-03-06 09:59:25 +0000

    me

--- main/messageCommands/auto.js
+++ main/messageCommands/auto.js
@@ -11,7 +11,7 @@ module.exports = function (msg, tokens, prefix, otfn) {
     } else if (badwords.some(ai => keywords.includes(ai))) {
         msg.delete();
         msg.reply(`Please, don't use bad words.`)
-    } else if (tokens.includes('like') || tokens.includes('love') || tokens.includes('adore') || tokens.includes('cute')) {
+    } else if (tokens.includes('love') || tokens.includes('adore') || tokens.includes('cute')) {
         msg.react(otfn.reac())
     } else if (msg.attachments.size > 0) {
         msg.react(otfn.reac())
