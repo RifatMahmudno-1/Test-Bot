855de1a9c5dd8fdc0ca1679138b5b04eab31f782
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-03-10 06:09:16 +0000

    me

--- main/keepalive.js
+++ main/keepalive.js
@@ -8,16 +8,27 @@ module.exports = function() {
   }).listen(3000);
 
   let url = `https://Test-Bot.rifatno1.repl.co`
-  setInterval(() => {
-    fetch(url)
-      .then(res => res.text())
-  }, 60000)
+  function auto() {
+    setInterval(() => {
+      fetch(url)
+        .then(res => res.text())
+    }, 60000)
+  }
+
+  fetch(url).then(res => res.text()).then(okay =>{
+    if (okay === `Keep Alive! Don't die.`) {
+      auto()
+    }else{
+      console.log(`Example app listening at http://localhost:3000`)
+    }
+  })
+  .catch(function(){console.log('Some error has occured in http request.')})
 }
   /*const express = require("express");
-  const app = express();
-  const port = 3000;
-  app.get("/", (req, res) => res.send("Keep Alive! Don't die."));
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => res.send("Keep Alive! Don't die."));
 
-  app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`)
-  )*/
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+)*/
