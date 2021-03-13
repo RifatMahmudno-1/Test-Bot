c0183ba129f185be17e17f293665cddf0856f78b
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-03-13 23:54:53 +0600

    me

--- main/autosend.js
+++ main/autosend.js
@@ -5,40 +5,53 @@ module.exports = function (auto, Discord) {
 
     function Time() {
         let date = new Date();
-        var hour = date.getHours()
-        return hour
+        var hour = date.getHours();
+        var minute = date.getMinutes();
+        return [hour, minute]
     }
 
     //quote
     function Quote(guilds) {
         guilds.forEach(function (el) {
-            if (Time() + 6 == 18)
+            if (Time()[0] + 6 == 18)
                 quote(fetch, Discord, el.channels.cache.find(i => i.type === `text`), 'https://zenquotes.io/api/today')
         })
     }
+
     //greetings
     function Greeting(guilds) {
         guilds.forEach(function (el) {
             var aaa = el.channels.cache.find(i => i.type === `text`);
-            if (Time() + 6 == 8) {
+            if (Time()[0] + 6 == 8) {
                 aaa.send(`Good Morning, everyone. Hope you will have a great day.`)
-            } else if (Time() + 6 == 16) {
+            } else if (Time()[0] + 6 == 16) {
                 aaa.send(`Good Afternoon, everyone.`)
-            } else if (Time() + 6 == 18) {
+            } else if (Time()[0] + 6 == 18) {
                 aaa.send(`Good Evening, everyone.`)
-            } else if (Time() + 6 == 0) {
+            } else if (Time()[0] + 6 == 0) {
                 aaa.send(`Good night, everyone. Now, stop scrolling the internet and go to bed. Don't worry. I'll stay awake on yout behalf for this server's safety.`).then(r => r.react('😁'))
             }
         })
     }
+
     //call immediately
     Quote(guilds);
-    Greeting(guilds)
+    Greeting(guilds);
+
+    //interval 1 minute
+    let minINT = setInterval(() => {
+        if (Time()[1] >= 5) {
+            hourINT(); //start hourINT
+            clearInterval(minINT) //clear minINT
+        }
+    }, 60000);
 
     //interval 1 hour
-    setInterval(() => {
-        guilds = auto.guilds.cache.map(r => r)
-        Quote(guilds)
-        Greeting(guilds)
-    }, 3600000);
+    function hourINT() {
+        setInterval(() => {
+            guilds = auto.guilds.cache.map(r => r)
+            Quote(guilds);
+            Greeting(guilds);
+        }, 3600000);
+    }
 }
