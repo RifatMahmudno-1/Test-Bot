95a043ba098bf72b6a8fe2dfefa568951c5aec72
Rifat Mahmud <>
2021-04-11 15:50:52 +0000

    me

--- main/messageCommands/clear.js
+++ main/messageCommands/clear.js
@@ -4,10 +4,12 @@ module.exports = async function (msg, tokens) {
             limit: 2
         }).then(results => {
             msg.channel.bulkDelete(results)
+            .catch(bulkerr)
         })
     } else if (msg.member.hasPermission('ADMINISTRATOR') && tokens[1] === 'all') {
         msg.channel.messages.fetch().then(results => {
             msg.channel.bulkDelete(results)
+            .catch(bulkerr)
         })
     } else if (tokens[1] >= 1 && tokens[1] <= 5) {
         let xy = parseInt(tokens[1]) + 1
@@ -15,6 +17,7 @@ module.exports = async function (msg, tokens) {
             limit: xy
         }).then(results => {
             msg.channel.bulkDelete(results)
+            .catch(bulkerr)
         })
     } else if (msg.member.hasPermission('ADMINISTRATOR') && tokens[1] > 5 && tokens[1] <= 80) {
         let xy = parseInt(tokens[1]) + 1
@@ -22,10 +25,14 @@ module.exports = async function (msg, tokens) {
             limit: xy
         }).then(results => {
             msg.channel.bulkDelete(results)
+            .catch(bulkerr)
         })
     } else if(msg.member.hasPermission('ADMINISTRATOR') && tokens[1] > 5 && tokens[1] > 80) {
         msg.reply(`You can only delete maximum of 80 messages at once.`)
     }else{
       msg.reply(`I think you don't have permission to delete lot's of messages. Only admins are permitted.`)
+    }
+    function bulkerr(){
+      msg.reply('I can only delete multiple messages when the messages are under 14 days old.')
     }
 }
