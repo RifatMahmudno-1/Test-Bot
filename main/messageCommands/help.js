eb029b9318cba736b9b0ab5211d9b80ea123c284
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-04-30 12:04:01 +0600

    me

--- main/messageCommands/help.js
+++ main/messageCommands/help.js
@@ -14,27 +14,22 @@ module.exports = function (msg, tokens, prefix, Discord) {
             name: '🌟' + xx('help') + ' and ' + xx('help here'),
             value: `
             🔰${xx('help')} => Get help in DM channel
-            🔰${xx('help here')} => Get help in this channel
-            `,
+            🔰${xx('help here')} => Get help in this channel`,
         }, {
             name: '🌟' + xx('gif') + ' and ' + xx('gif <search>'),
             value: `
             🔰${xx('gif')} => Get a random anime gif.
-            🔰${xx('gif search')} => Get a random gif from your search. **${xx('gif cute kitten')}** => get a cute kitten gif.
-            `,
+            🔰${xx('gif search')} => Get a random gif from your search. **${xx('gif cute kitten')}** => get a cute kitten gif.`,
         }, {
             name: '🌟' + xx('clear') + ' and ' + xx('clear <how many>') + ' and ' + xx('clear all'),
             value: `
             🔰${xx('clear')}=> delete previous 1 message.
             🔰${xx('clear <how many>')}=>**${xx('clear 50')}** to delete pevious 50 messages.
-            🔰${xx('clear all')}=> Delete previous several messages.
-            ***You can't delete messages which are older than 14days.***
-            `,
+            🔰${xx('clear all')}=> Delete previous several messages.`,
         }, {
             name: '🌟' + xx('bhf <message>'),
             value: `
-            🔰The bot will send your message on your behalf. **${xx("bhf I'm well")}}** => The bot will send I'm well.
-            `,
+            🔰The bot will send your message on your behalf. **${xx("bhf I'm well")}}** => The bot will send I'm well.`,
         }, {
             name: '🌟' + xx('avt') + ' and ' + xx('avt <@mention users>'),
             value: `
@@ -47,40 +42,36 @@ module.exports = function (msg, tokens, prefix, Discord) {
             name: '🌟' + xx('kick <@mention users whom you want to kick>') + ' and ' + xx('ban <@mention users whom you want to ban>'),
             value: `
             🔰${xx('kick <@mention users whom you want to kick>')} => The bot will kick the mentioned members one by one.
-            🔰${xx('ban <@mention users whom you want to ban>')} => The bot will ban the mentioned members one by one.
-            `,
+            🔰${xx('ban <@mention users whom you want to ban>')} => The bot will ban the mentioned members one by one.`,
         }, {
             name: '🌟' + xx('snick <@mention user> <nickname>') + ' and ' + xx('rnick <mention users>'),
             value: `
             🔰${xx('snick <@mention user> <nickname>')} => Type **${xx('snick @Alex Gamer @Helper Bot')}** to set Alex's nickname to Gamer and Helper's nickname to Bot. You can add more or less members.
-            🔰${xx('rnick <mention users>')} => The bot will remove the nicknames of the mentioned users.
-            `,
+            🔰${xx('rnick <mention users>')} => The bot will remove the nicknames of the mentioned users.`,
         }, {
             name: '🌟' + xx('roles') + ' and ' + xx('roles add <mention roles> <mention users>') + ' and ' + xx('roles remove <mention roles> <mention users>'),
             value: `
             🔰${xx('roles')} => Get a list of all available roles.
-            🔰${xx('roles add <mantion roles> <mantion users>')} => Give the mentioned users the mentioned roles. **${xx('roles add @admin @mod @Alex @Helper')}** to give Alex and Helper the mod and Admin role. You can add more or less roles and member.
-            🔰${xx('roles remove <mantion roles> <mantion users>')} => Remove the mentioned roles from the mentioned users. **${xx('roles remove @admin @mod @Alex @Helper')}** to remove the mod and Admin role from Alex and Helper. You can add more or less roles and member.`,
+            🔰${xx('roles add <mantion roles> <mantion users>')} => Give the mentioned users the mentioned roles. **${xx('roles add @admin @mod @Alex @Helper')}** to give Alex and Helper the mod and Admin role. 
+            🔰${xx('roles remove <mantion roles> <mantion users>')} => Remove the mentioned roles from the mentioned users. **${xx('roles remove @admin @mod @Alex @Helper')}** to remove the mod and Admin role from Alex and Helper.
+            **You can add more or less roles and member.**`,
         }, {
             name: '🌟' + xx('mem all') + ' and ' + xx('mem roles') + ' and ' + xx('mem names') + ' and ' + xx('mem ids'),
             value: `
             🔰${xx('mem all')} => get list of all members' name, id and roles.
             🔰${xx('mem roles')} => get list of all members' roles.
             🔰${xx('mem names')} => get all members' name.
-            🔰${xx('mem ids')} => get all members' id.
-            `,
+            🔰${xx('mem ids')} => get all members' id.`,
         }, {
             name: '🌟' + xx('abt') + ' and ' + xx('abt <mention users>'),
             value: `
             🔰${xx('abt')} => Get info about this Guild/Server.
-            🔰${xx('abt <mention users>')} => Get mentioned user's info of this guild. **${xx('abt @Helper @Rifat')}** to get Helper's and Rifat's info. You can add more or less members
-            `,
+            🔰${xx('abt <mention users>')} => Get mentioned user's info of this guild. **${xx('abt @Helper @Rifat')}** to get Helper's and Rifat's info. You can add more or less members`,
         }, {
             name: '🌟' + xx('rank') + ' and ' + xx('rank <@mention users>'),
             value: `
             🔰${xx('rank')} => get info about your total sent messages, level and rank etc.
-            🔰${xx('rank <@mention users>')} => get info about mention users' total sent messages, level and rank etc. such as=> **${xx('rank @Rifat @Alex @Helper')}**
-            `,
+            🔰${xx('rank <@mention users>')} => get info about mention users' total sent messages, level and rank etc. such as=> **${xx('rank @Rifat @Alex @Helper')}**`,
         }, {
             name: '🌟' + xx('rd word'),
             value: `🔰You can only get memes or posts on meme, anime, cosplay, thought, wallpaper, art, pic. Like for meme type **${xx('rd meme')}**. All the contents are from reddit.`,
@@ -88,15 +79,13 @@ module.exports = function (msg, tokens, prefix, Discord) {
             name: '🌟' + xx('gimg <search>') + ' and ' + xx('quote'),
             value: `
             🔰${xx('gimg <search>')} =>Get first google image from that search and send it. such as- **${xx('gimg cat')}** to get a image of cat.
-            🔰${xx('quote')} => Get a random Quote.
-            `,
+            🔰${xx('quote')} => Get a random Quote.`,
         }, {
             name: '🌟' + xx('slow <time in seconds> <reason>') + ' and ' + xx('slow 0') + ' and ' + xx('slow off'),
             value: `
             🔰${xx('slow <time in seconds> <reason>')} => Type **${xx('slow 5 to avoid spamming')}** to limit one message every 5 seconds. Here reason is 'to avoid spamming'. Reason isn't necessary.
             🔰${xx('slow 0')} => Stop slow mode.
-            🔰${xx('slow off')} => Stop slow mode.
-            `,
+            🔰${xx('slow off')} => Stop slow mode.`,
         }, {
             name: '🌟' + xx('a <anime\'s name>') + ' or ' + xx('anime <anime\'s name>') + ' and ' + xx('m <manga\'s name>') + ' or ' + xx('manga <manga\'s name>') + ' and ' + xx('anicharacter <character\'s name>') + ' and ' + xx('aniuser <username>') + ' and ' + xx("anistaff <staff's name>") + ' and ' + xx('anistudio <studio\'s name>'),
             value: `
@@ -106,8 +95,7 @@ module.exports = function (msg, tokens, prefix, Discord) {
             🔰${xx('aniuser <username>') } => Get info of that anilist user. Such as=> **${xx('aniuser rifatal')}**
             🔰${xx("anistaff <staff's name>")} => Get info of that anime/ manga writer, voice actor, director etc. Such as=> **${xx('anistaff Hiroshi Kamiya')}**
             🔰${xx('anistudio <studio\'s name>')} => Get info of that anime Studio. Such as=> **${xx('anistudio MAPPA')}**
-            ***ALL DATA IS FROM ANILIST***
-            `,
+            ***ALL DATA IS FROM ANILIST***`,
         }, {
             name: '🌟' + xx('ytplay <video title>') + ' and ' + xx('ytplay <video url>') + ' and ' + xx('ytstop'),
             value: `
@@ -115,15 +103,15 @@ module.exports = function (msg, tokens, prefix, Discord) {
             🔰${xx("ytplay <video url>")} => Play youtube video from video url/link on a voice channel (Audio Only). Example- Type **${xx('ytplay https://www.youtube.com/watch?v=kJQP7kiw5Fk')}** to play Despacito music/video.
             🔰${xx("ytstop <ytstop>")} => Stop playing music/video in voice channel.`
         })
-        .addField('Automated', 'Bot will autimatically respond if someone types hi, hello, hlw, bye, Goodbye according to GMT +6. Bot will auto delete any offensive words. Bot will autimatically respond if someone is added or kicked or banned from this server. Bot will autimatically react to certain messages. Bot will automatic send a quote a day.', false)
-        .addField('Reaction info', 'In those messages where bot has reacted ❌, there if you click on that reaction and increase the number then the message will be deleted. After 8second the bot will auto remove that emoji.', false)
+        .addField('Automated', 'Bot will autimatically respond if someone types hi, hello, hlw, bye, Goodbye according to GMT +6. Bot will auto delete any offensive words and respond if someone is added or kicked or banned from this server. Bot will autimatically react to certain messages and automatic send a quote a day.', false)
+        .addField('Reaction info', 'click on ❌ react to remove that message. Otherwise, react will be removed in 8 seconds.', false)
         .addField('Add this bot in your server', `[Click and authorize pemission to add this bot in your server](https://discord.com/api/oauth2/authorize?client_id=812537560030117928&permissions=8&scope=bot)`, false)
         .setFooter('Have a nice time');
     if (tokens[1] === 'here') {
         msg.channel.send(embed)
     } else {
         if (msg.channel.id !== '813476929268351007') {
-            msg.reply(`Check. You have received a message from me. Everything is written there.`)
+            msg.reply(`Check. You have received a message from me. Everything is written there.`).then(r => r.react('💖'))
         }
         msg.author.send(embed);
     }
