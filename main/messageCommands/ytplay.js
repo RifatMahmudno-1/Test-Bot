06fc782885df0a14a49bfb41872ec95d2738bbf7
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-04-30 17:19:17 +0600

    me

--- main/messageCommands/ytplay.js
+++ main/messageCommands/ytplay.js
@@ -7,23 +7,15 @@ module.exports = async function (msg, Discord) {
     if (!voiceChannel) return msg.channel.send('You need to be in a channel to play this music');
     let args = tokens.slice(1).join(' ')
     //get video
-    if (args.slice(0, 8) === 'https://' || args.slice(0, 7) === 'http://' || args.slice(0, 7) === 'www.you') {
-        yts({
-                videoId: args.match(/=[\w+-_+=]+/g)[0].slice(1)
-            })
-            .then((video) => {
-                playVid([video.url, video.title, video.author.name, video.timestamp, video.seconds])
-            })
-            .catch(() => {
-                msg.channel.send(`Couldn't find any video. Please provide correct title or url and try again.`)
-            })
-    } else {
-        let getVids = await yts(args);
-        if (getVids.videos.length < 1) return msg.channel.send(`Couldn't find any video. Please provide correct title or url and try again.`);
-        let video = getVids.videos[0]
-        playVid([video.url, video.title, video.author.name, video.timestamp, video.seconds])
-    }
-
+    yts(args)
+        .then((getVids) => {
+            let video = getVids.all.filter(x => x.type === 'live' || x.type === 'video')[0]
+            if (video.length < 1) return msg.channel.send(`Couldn't find any video. Please provide correct title or url and try again.`);
+            playVid([video.url, video.title, video.author.name, video.timestamp, video.seconds || 0])
+        })
+        .catch(() => {
+            msg.channel.send(`Couldn't find any video. Please provide correct title or url and try again.`)
+        })
     async function playVid(xx) {
         if (xx[4] === 0) {
             msg.channel.send(`Playing music "**${xx[1]}**" from "**${xx[2]}**" YouTube Channel. **It's a live stream**. *Starting may take a few seconds.*`)
@@ -42,7 +34,8 @@ module.exports = async function (msg, Discord) {
                 msg.channel.send('*Left the voice channel because none was there.*').then(r => r.react('😭'))
             }
         }, 60000);
-        connection.play(stream, {
+        connection
+            .play(stream, {
                 type: 'opus',
                 seek: 0,
                 volume: 1
@@ -66,5 +59,22 @@ module.exports = async function (msg, Discord) {
                 voiceChannel.leave();
                 msg.channel.send(`Full music played "${xx[1]}" from ${xx[2]} YouTube Channel.`);
             })*/
+
+        /*if (args.slice(0, 8) === 'https://' || args.slice(0, 7) === 'http://' || args.slice(0, 7) === 'www.you') {
+        yts({
+                videoId: args.match(/=[\w+-_+=]+/g)[0].slice(1)
+            })
+            .then((video) => {
+                playVid([video.url, video.title, video.author.name, video.timestamp, video.seconds])
+            })
+            .catch(() => {
+                msg.channel.send(`Couldn't find any video. Please provide correct title or url and try again.`)
+            })
+    } else {
+    let getVids = await yts(args);
+    let video = getVids.all.filter(x => x.type === 'live' || x.type === 'video')[0]
+    if (video.length < 1) return msg.channel.send(`Couldn't find any video. Please provide correct title or url and try again.`);
+    playVid([video.url, video.title, video.author.name, video.timestamp, video.seconds || 0])
+    }*/
     }
 }
