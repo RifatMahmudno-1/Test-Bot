53aee22d94e5bbd84960c482da55d007758a1d0e
Rifat Mahmud <rifatmahmudpc@gmail.com>
2021-05-23 22:32:29 +0600

    gimg fix

--- main/messageCommands/googleimage.js
+++ main/messageCommands/googleimage.js
@@ -1,36 +1,31 @@
-//https://github.com/jimkang/g-i-s
-//https://www.npmjs.com/package/google-images
-require('dotenv').config();
-
 const GoogleImages = require('google-images');
 const google = new GoogleImages(`${process.env.GOOGLEID}`, `${process.env.GOOGLEAPI}`);
 module.exports = function (msg, tokens, prefix) {
     if (tokens.length > 1) {
         let keywords = tokens.slice(1, tokens.length).join(' ')
         var random = Math.floor(Math.random() * 10)
-        google.search(keywords)
-            .then(images => {
-                msg.channel.send(keywords).then()
-                msg.channel.send(images[random].url)
-            }).catch(() => {
-                msg.channel.send(`Some errors have occured. Please type correctly.`)
-            });
+        if (msg.channel.nsfw) {
+            google.search(keywords)
+                .then(images => {
+                    msg.channel.send(keywords)
+                        .then(() => msg.channel.send(images[random].url))
+                }).catch(() => {
+                    msg.channel.send(`Some errors have occured. Please type correctly.`)
+                });
+        } else {
+            google.search(keywords, {
+                    safe: "active"
+                })
+                .then(images => {
+                    if (!images || images.length == 0) return msg.channel.send(`Couldn't find any safe image. If you are searching bad images use nsfw channel.`)
+                    msg.channel.send(keywords)
+                        .then(() => msg.channel.send(images[random].url))
+                }).catch(() => {
+                    msg.channel.send(`Some errors have occured. Please type correctly.`)
+                });
+        }
     } else {
         msg.reply(`Please type correctly. For help type '${prefix}help'`)
     }
 
 }
-/*
-[{
-    "url": "http://steveangello.com/boss.jpg",
-    "type": "image/jpeg",
-    "width": 1024,
-    "height": 768,
-    "size": 102451,
-    "thumbnail": {
-        "url": "http://steveangello.com/thumbnail.jpg",
-        "width": 512,
-        "height": 512
-    }
-}]
- */
